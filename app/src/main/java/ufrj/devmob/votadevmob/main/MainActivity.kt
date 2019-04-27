78518ff7f45aa204e762141c20f64c606ac06805
VictorGTdaCruz <victorgtdacruz@outlook.com>
2019-04-27 14:02:55 -0300

    :construction: Add contract + activity and layout

--- app/src/main/java/ufrj/devmob/votadevmob/main/MainActivity.kt
+++ app/src/main/java/ufrj/devmob/votadevmob/main/MainActivity.kt
@@ -2,19 +2,62 @@ package ufrj.devmob.votadevmob.main
 
 import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.dialog_main_input.view.*
 import ufrj.devmob.votadevmob.R
+import ufrj.devmob.votadevmob.core.model.Poll
 import ufrj.devmob.votadevmob.poll.PollActivity
+import ufrj.devmob.votadevmob.result.PollResultActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MainContract.View {
+
+    internal lateinit var presenter: MainContract.Presenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        createPollButton.setOnClickListener { startActivity(Intent(this, CreatePollActivity::class.java)) }
-        voteButton.setOnClickListener { startActivity(Intent(this, PollActivity::class.java)) }
-//        resultPollButton.setOnClickListener { startActivity(Intent(this, PollResultActivity::class.java)) }
+        presenter = MainPresenter(this)
+
+        createPollButton.setOnClickListener { showToastError("Em breve") }
+        voteButton.setOnClickListener { showInputDialog(PollActivity::class.java) }
+        resultPollButton.setOnClickListener { showInputDialog(PollResultActivity::class.java) }
+    }
+
+    private fun showInputDialog(activity: Class<out AppCompatActivity>) {
+        val view = layoutInflater.inflate(R.layout.dialog_main_input, null)
+        AlertDialog.Builder(this).run {
+            setView(view)
+            setTitle("Qual votação?")
+            setPositiveButton("OK") { _, _ ->
+                presenter.getPollForActivity(id = view.idEditText.text.toString(),
+                    password = view.passwordEditText.text.toString(),
+                    activity = activity)
+            }
+            setNegativeButton("Cancel") { _, _ -> }
+            show()
+        }
+    }
+
+    override fun goToActivity(activity: Class<out AppCompatActivity>, poll: Poll) {
+        startActivity(Intent(this, activity).putExtra("current_poll", poll))
+    }
+
+    override fun showLoading() {
+        mainContent.visibility = View.GONE
+        mainLoading.visibility = View.VISIBLE
+    }
+
+    override fun hideLoading() {
+        mainContent.visibility = View.VISIBLE
+        mainLoading.visibility = View.GONE
+    }
+
+    override fun showToastError(errorMessage: String) {
+        Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/ufrj/devmob/votadevmob/main/MainContract.kt
+++ app/src/main/java/ufrj/devmob/votadevmob/main/MainContract.kt
@@ -1,18 +1,19 @@
 package ufrj.devmob.votadevmob.main
 
+import android.support.v7.app.AppCompatActivity
+import ufrj.devmob.votadevmob.core.model.Poll
+
 interface MainContract {
 
     interface View {
-        fun setupPollLayout(title: String, options: List<String>)
+        fun goToActivity(activity: Class<out AppCompatActivity>, poll: Poll)
         fun showLoading()
         fun hideLoading()
-        fun showToastSuccess()
         fun showToastError(errorMessage: String)
-        fun showMajorErrorMessage()
     }
 
     interface Presenter {
-        fun getPoll(id: String, password: String)
+        fun getPollForActivity(id: String, password: String, activity: Class<out AppCompatActivity>)
     }
 
 }
