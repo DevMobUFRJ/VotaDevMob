7e4d9538899db05287159adadbf0853be8a55ff2
VictorGTdaCruz <victorgtdacruz@outlook.com>
2019-04-27 14:02:01 -0300

    :construction: Add presenter

--- app/src/main/java/ufrj/devmob/votadevmob/main/MainPresenter.kt
+++ app/src/main/java/ufrj/devmob/votadevmob/main/MainPresenter.kt
@@ -1,19 +1,27 @@
 package ufrj.devmob.votadevmob.main
 
-import ufrj.devmob.votadevmob.Callback
+import android.support.v7.app.AppCompatActivity
+import ufrj.devmob.votadevmob.core.callback.Callback
+import ufrj.devmob.votadevmob.core.model.Poll
 import java.lang.Exception
 
 class MainPresenter(private val view: MainContract.View) : MainContract.Presenter {
 
     internal val model by lazy { MainModel() }
 
-    override fun getPoll(id: String, password: String) {
-        model.getPoll(pollId = id, password = password, callback = object : Callback {
-            override fun onSuccess() {
-
+    override fun getPollForActivity(id: String, password: String, activity: Class<out AppCompatActivity>) {
+        view.showLoading()
+        model.getPoll(pollId = id, callback = object : Callback<Poll> {
+            override fun onSuccess(result: Poll) {
+                view.hideLoading()
+                if (result.password == password)
+                    view.goToActivity(activity, result)
+                else
+                    view.showToastError("Incorrect Password")
             }
             override fun onError(exception: Exception) {
-
+                view.hideLoading()
+                view.showToastError(exception.message.toString())
             }
         })
     }
